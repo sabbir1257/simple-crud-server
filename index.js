f943083b0b22b2a354c7c65d98650c48769f17fb
sabbir1257 <masabbir1257@gmail.com>
2025-02-27 13:00:32 +0600

    Load Single Item By Id And create Update route

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user)
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log("new user", user);
@@ -40,10 +47,10 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/users/:id", async(req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       console.log("please delete from database", id);
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
 
       const result = await userCollection.deleteOne(query);
       res.send(result);
