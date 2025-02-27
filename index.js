c9bab822b4fa6487d3118884a3d428e1dbd19e74
sabbir1257 <masabbir1257@gmail.com>
2025-02-27 10:42:41 +0600

    Delete A User From the Database using UI click button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5001;
 const app = express();
 
@@ -38,6 +38,15 @@ async function run() {
       const user = req.body;
       console.log("new user", user);
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async(req, res) => {
+      const id = req.params.id;
+      console.log("please delete from database", id);
+      const query = {_id: new ObjectId(id)}
+
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
 
